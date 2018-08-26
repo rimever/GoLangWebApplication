2627108040aac652a2eea6d908ad3a217f2bf983
rimever <mot_siva_ordin@hotmail.com>
2018-08-26 15:50:09 +0900

    golandから実行すると、カレントディレクトリが異なってエラーとなる。 パスを取得して、テンプレートを取得するようにした。

--- chat/main.go
+++ chat/main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte(`
-<html>
-	<head>
-		<title>チャット</title>
-	</head>
-	<body>
-		チャットしましょう！
-	</body>
-</html>
-`))
+// templateは1つのテンプレートを表します。
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	template *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		currentDirectoryPath, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		t.template = template.Must(template.ParseFiles(filepath.Join(currentDirectoryPath+"/chat/templates", t.filename)))
 	})
+	// 戻り値はチェックすべき
+	result := t.template.Execute(w, nil)
+	log.Print(result)
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	// Webサーバー開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
